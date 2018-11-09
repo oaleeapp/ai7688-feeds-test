6fc5431923ba8fbd4aacd6bc583c9f4ccdd8e488
PraneshBalekai <praneshbalekai@gmail.com>
2018-11-09 19:18:58 +0800

    Uploading #2

--- oya_package/files/Oya/uploading.sh
+++ oya_package/files/Oya/uploading.sh
@@ -5,15 +5,14 @@ uploadRootDirectory="/root/uploadAudio"
 uploadSDDirectory="/Media/SD-P1/uploadAudio"
 
 uploadURL=`uploading uploadURL get`
-
 while true
 do
     list=$(ls ${uploadRootDirectory}/*.wav 2>/dev/null)
-    
+
     if [ "$list" != "" ]; then
       for fullname in $list; do
         file=${fullname##*/}
-        ret=$(curl --data-binary @/root/uploadAudio/${file} --write-out "%{http_code}" -H 'Content-Type: audio/wav' $uploadURL${file})
+        ret=$(curl --data-binary @/root/uploadAudio/${file} --write-out "%{http_code}" -H 'Content-Type: audio/wav' ${uploadURL}${file})
         if [ "$ret" != "200" ]; then
           rm $fullname
         else
@@ -29,7 +28,7 @@ do
     if [ "$list" != "" ]; then
       for fullname in $list; do
         file=${fullname##*/}
-        ret=$(curl --data-binary @/root/uploadAudio/${file} --write-out "%{http_code}" -H 'Content-Type: audio/wav' $uploadURL${file})
+        ret=$(curl --data-binary @/root/uploadAudio/${file} --write-out "%{http_code}" -H 'Content-Type: audio/wav' ${uploadURL}${file})
         if [ "$ret" != "200" ]; then
           rm $fullname
         else
