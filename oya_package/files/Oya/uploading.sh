3b1dfc04b113707ff357d3c5fad150d8bd6071c7
PraneshBalekai <praneshbalekai@gmail.com>
2018-11-09 18:11:13 +0800

    Recording done. Uploading #1

---
+++ oya_package/files/Oya/uploading.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+(
+uploadRootDirectory="/root/uploadAudio"
+uploadSDDirectory="/Media/SD-P1/uploadAudio"
+
+uploadURL=`uploading uploadURL get`
+
+while true
+do
+    list=$(ls ${uploadRootDirectory}/*.wav 2>/dev/null)
+    
+    if [ "$list" != "" ]; then
+      for fullname in $list; do
+        file=${fullname##*/}
+        ret=$(curl --data-binary @/root/uploadAudio/${file} --write-out "%{http_code}" -H 'Content-Type: audio/wav' $uploadURL${file})
+        if [ "$ret" != "200" ]; then
+          rm $fullname
+        else
+          rm $fullname
+          lastUploadedTime=`date +%s`
+          uploading lastUploadedTime set ${lastUploadedTime}
+        fi
+      done
+    fi
+
+    list=$(ls ${uploadSDDirectory}/*.wav 2>/dev/null)
+
+    if [ "$list" != "" ]; then
+      for fullname in $list; do
+        file=${fullname##*/}
+        ret=$(curl --data-binary @/root/uploadAudio/${file} --write-out "%{http_code}" -H 'Content-Type: audio/wav' $uploadURL${file})
+        if [ "$ret" != "200" ]; then
+          rm $fullname
+        else
+          rm $fullname
+          lastUploadedTime=`date +%s`
+          uploading lastUploadedTime set ${lastUploadedTime}
+        fi
+      done
+    fi
+done
+) & uploading pid set $!
