969df188fee78e50f1f534afb299a96de19c024b
PraneshBalekai <praneshbalekai@gmail.com>
2018-11-21 12:30:34 +0800

    uart process done

--- oya_package/files/Oya/uart/uart_process.sh
+++ oya_package/files/Oya/uart/uart_process.sh
@@ -4,5 +4,22 @@
 while True
 do
   uartcommand=`uart read`
+  #to see if there are any commands from the MCU
+  #Possible commands
+  #0x01 - mute
+  #0x02 - poweroff
+  if [ ${uartcommand} & \x01 ]; then
+    muteState=`state mute get`
+    if [ ${muteState} == "1" ]; then
+      state mute set 0
+      state mute commit
+    else
+      state mute set 1
+      state mute commit
+    fi
+  elif [ ${uartcommand} & \x02 ]; then
+    #initiate power off sequence
+    poweroff
+  fi
 done
 ) & uart process pid set $!
