fc9d5dbb8d49a0cd12a2754dd1e02c7f8f3f80e0
PraneshBalekai <praneshbalekai@gmail.com>
2018-11-21 14:17:23 +0800

    uart #3

--- oya_package/files/Oya/uart/uart_process.sh
+++ oya_package/files/Oya/uart/uart_process.sh
@@ -8,7 +8,7 @@ do
   #Possible commands
   #0x01 - mute
   #0x02 - poweroff
-  if [ ${uartcommand} & \x01 ]; then
+  if [ ${uartcommand} & 1 == 1 ]; then
     muteState=`state mute get`
     if [ ${muteState} == "1" ]; then
       state mute set 0
@@ -17,7 +17,7 @@ do
       state mute set 1
       state mute commit
     fi
-  elif [ ${uartcommand} & \x02 ]; then
+  elif [ ${uartcommand} & 2 == 2 ]; then
     #initiate power off sequence
     poweroff
   fi
