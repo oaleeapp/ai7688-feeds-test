276df27b57287048a5ba8b0a487e0eef6a602c75
PraneshBalekai <praneshbalekai@gmail.com>
2018-11-23 15:32:52 +0800

    flask_server update #2

--- oya_package/files/Oya/flask_server/get_aplist.sh
+++ oya_package/files/Oya/flask_server/get_aplist.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 iwpriv ra0 set SiteSurvey=1
-sleep 5
+sleep 1
 iwpriv ra0 get_site_survey | sed 1d | while read line;
 do
-  echo $line > /Oya/flask_server/APLIST.txt
+  echo $line >> /Oya/flask_server/APLIST.txt
 done
