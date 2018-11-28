4694fb87ccafc0f27eef5fceca15e4ed6bc4520a
PraneshBalekai <praneshbalekai@gmail.com>
2018-11-28 15:54:00 +0800

    uart bugs fixreed

--- oya_package/files/Oya/uart/uart_process.sh
+++ oya_package/files/Oya/uart/uart_process.sh
@@ -8,18 +8,21 @@ do
   #Possible commands
   #0x01 - mute
   #0x02 - poweroff
-  if [ $((${uartcommand} & 1)) == 1 ]; then
+  if [ $((${uartcommand} & 1 )) == 1 ]; then
     muteState=`state mute get`
     if [ ${muteState} == "1" ]; then
-      state mute set 0
-      state mute commit
+      #state mute set 0
+      #state mute commit
+      echo "mute toggle to 0"
     else
-      state mute set 1
-      state mute commit
+      #state mute set 1
+      #state mute commit
+      echo "mute toggle to 1"
     fi
-  elif [ $((${uartcommand} & 2)) == 2 ]; then
+  elif [ $((${uartcommand} & 2 )) == 2 ]; then
     #initiate power off sequence
-    poweroff
+    #poweroff
+    echo "poweroff"
   fi
 done
 ) & uart process pid set $!
