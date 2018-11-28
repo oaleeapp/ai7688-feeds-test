eeb97e08ea4c0ce4c81782f45acc7c1c96be2781
PraneshBalekai <praneshbalekai@gmail.com>
2018-11-28 17:21:51 +0800

    uart rx test with python

---
+++ oya_package/files/Oya/uart/uart_process.py
@@ -0,0 +1,19 @@
+import serial
+import subprocess
+import time
+
+ser=serial.Serial('/dev/ttyS0',115200)
+
+def main():
+    while (1):
+        input=ser.read()
+        if(int(input) == 1):
+            print("mute")
+            #subprocess.call(['state','mute','set','1'])
+            #subprocess.call(['state','mute','commit'])
+        if(int(input) == 2):
+            print("poweroff")
+            #subprocess.call(['poweroff'])
+
+if __name__ == "__main__":
+    main()
--- oya_package/files/Oya/uart/uart_recieve.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-(cat /dev/ttyS0 > /Oya/uart/uartcommand) & uart recieve pid set $!
