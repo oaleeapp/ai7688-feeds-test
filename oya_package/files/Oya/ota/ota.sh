72c9ad635f2466b2cc7b9f52de5f82158d170c05
PraneshBalekai <praneshbalekai@gmail.com>
2018-12-03 15:23:04 +0800

    OTA firstirst commit

---
+++ oya_package/files/Oya/ota/ota.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+otaurl=`ota url get`
+
+#downlaod the .bin file
+wget ${otaurl} -O /tmp/openwrt-ramips-mt7688-LinkIt7688-squashfs-sysupgrade.bin
+
+#stop all running processes
+/etc/init.d/recording stop
+/etc/init.d/uploading stop
+/etc/init.d/mqtt stop
+/etc/init.d/flask_server stop
+/etc/init.d/internet_status stop
+/etc/init.d/uart stop
+
+#save relevent configuration to a .config file
+
+
+sleep 1
+#use sysupgrade to update the firmware
+sysupgrade -v /tmp/openwrt-ramips-mt7688-LinkIt7688-squashfs-sysupgrade.bin
